d9d189caf3ba10d39cd1c96856d32115d142cd7c
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-07-04 12:46:07 -0400

    added surge to BP: no more charge during spectator

---
+++ battlepass/Lua/battlepatch_surge.lua
@@ -0,0 +1,19 @@
+--- prevent surges from having properties while in spectator
+local buh = function(p)
+	if p.spectator and p.stt then
+		p.stt.eleccharging = 0, //max 105
+		p.stt.fullcharge = false,
+		p.stt.homer = false,
+		p.stt.cancelhomer = 0,
+		p.stt.voltjump = 0,
+		p.stt.voltdash = 0,
+		p.stt.supercharge = 0,
+		p.stt.voltframe = A,
+		p.stt.trailframe = A,
+		p.stt.newcharge = false,
+		p.stt.egirl = false,
+		p.stt.battlehoming = 0,
+		p.stt.killaura = false
+	end
+end
+addHook("PlayerThink", buh)
