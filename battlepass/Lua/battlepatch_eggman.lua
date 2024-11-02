782bd1ee2eb5decc3f3ae74942506cce25e5affd
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-11-01 23:36:03 -0400

    Eggman's badniks eventually explode while in goop

---
+++ battlepass/Lua/battlepatch_eggman.lua
@@ -0,0 +1,21 @@
+local function safeFreeslot(...)
+    for _, item in ipairs({...}) do
+        if rawget(_G, item) == nil then
+            freeslot(item)
+        end
+    end
+end
+
+safeFreeslot("MT_CRAWLAMISSILE", "MT_JETJAWMISSILE") -- Gotta love addon load order
+
+local function badnik_goop(mobj) -- ty metakimi (i modified it a bit to add effects)
+    if mobj.time > 270 and P_IsObjectInGoop(mobj) then
+        local explode = P_SpawnMobjFromMobj(mobj,0,0,0,MT_EXPLODE)
+        S_StartSound(explode, sfx_s3k3d)
+        P_RemoveMobj(mobj)
+    end
+end
+
+for n = MT_CRAWLAMISSILE,MT_JETJAWMISSILE
+    addHook("MobjThinker",badnik_goop,n)
+end
