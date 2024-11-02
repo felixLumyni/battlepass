7f3eb89a6329cc5e2e5539298c54975b222f599a
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-11-01 23:48:16 -0400

    Nvm his badniks will always explode after about 8s

--- battlepass/Lua/battlepatch_eggman.lua
+++ battlepass/Lua/battlepatch_eggman.lua
@@ -9,7 +9,7 @@ end
 safeFreeslot("MT_CRAWLAMISSILE", "MT_JETJAWMISSILE") -- Gotta love addon load order
 
 local function badnik_goop(mobj) -- ty metakimi (i modified it a bit to add effects)
-    if mobj.time > 270 and P_IsObjectInGoop(mobj) then
+    if mobj.time > 270 then
         local explode = P_SpawnMobjFromMobj(mobj,0,0,0,MT_EXPLODE)
         S_StartSound(explode, sfx_s3k3d)
         P_RemoveMobj(mobj)
