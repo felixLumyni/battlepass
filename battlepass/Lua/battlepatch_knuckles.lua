c3867320c1686b45497d2d6e65ac80a6a9e35288
Mari0shi <@mari0shi>
2024-12-01 10:00:08 -0500

    Glide SFX fixes

--- battlepass/Lua/battlepatch_knuckles.lua
+++ battlepass/Lua/battlepatch_knuckles.lua
@@ -35,16 +35,18 @@ addHook("PlayerThink", function(player)
     end
 
     local gliding = (player.pflags & PF_GLIDING)
-    local glidelaunch = (gliding and (player.glidetime < 1))
+    local glidelaunch = (gliding and not(player.mo.knux_glidelaunch))
 
     if gliding then
         if glidelaunch then
             teamSound(player.mo, player, sfx_nullba, GLIDE_LAUNCH, 255, selfisenemy)
+            player.mo.knux_glidelaunch = true
         end
         if not(S_SoundPlaying(player.mo, GLIDE_ACTIVE)) then
             teamSound(player.mo, player, sfx_nullba, GLIDE_ACTIVE, 255, selfisenemy)
         end
     else
+        player.mo.knux_glidelaunch = nil
         S_StopSoundByID(player.mo, GLIDE_ACTIVE)
         S_StopSoundByID(player.mo, GLIDE_ACTIVE)
     end
